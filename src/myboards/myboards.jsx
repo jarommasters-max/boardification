cd5cdcdbaca69cb46423949fac5831223eb7c5be
Jarom Masters <jarommasters@gmail.com>
2026-02-08 18:31:07 -0700

    Added placeholder code in .jsx files

---
+++ src/myboards/myboards.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Myboards() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Myboards displayed here</div>
+    </main>
+  );
+}
