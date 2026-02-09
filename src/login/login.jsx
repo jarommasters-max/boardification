f6661a914454d6136b4efebccbbcdb3bd48d788f
Jarom Masters <jarommasters@gmail.com>
2026-02-08 19:03:00 -0700

    Added login component

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main>
+        <h4>Login:</h4>
+        <form method="get" action="myboards.html">
+            <div>
+                <span>Email</span>
+                <input class="form-control" type="text" placeholder="exampleemail@examplemail.com"/>
+            </div>
+            <div>
+                <span>Password</span>
+                <input class="form-control" type="test" placeholder="***********"/>
+            </div>
+            <div class="buttons">
+            <button type="submit" class="btn btn-primary">Login</button>
+            <button type="submit" class="btn btn-outline-secondary">Create Account</button>
+            </div>
+        </form>
     </main>
   );
 }
