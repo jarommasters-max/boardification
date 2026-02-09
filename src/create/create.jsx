362b93475fe7e94b5b649cc6f382cbaf21e874d5
Jarom Masters <jarommasters@gmail.com>
2026-02-08 19:02:26 -0700

    added create component

--- src/create/create.jsx
+++ src/create/create.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 export function Create() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Create displayed here</div>
-    </main>
+    <main>
+        <h2>Create New Leaderboard:</h2>
+        
+        <form method="get" action="myboards.html">
+            <div>
+                <span>Board Title:</span>
+                <input type="text" class="form-control" placeholder="ex: Milk Drinking"/>
+            </div>
+            <div>
+                <span>Score Type:</span>
+                <input type="text" class="form-control" placeholder="ex: Litres of Milk"/>
+            </div>
+            <div class="buttons">
+            <button type="submit" class="btn btn-outline-primary">Create Board</button>
+            </div>
+        </form>
+        </main>
   );
 }
