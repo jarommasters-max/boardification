054cdf178a4bd30a8682037164f88278b8ff7902
Jarom Masters <jarommasters@gmail.com>
2026-02-23 13:56:44 -0700

    Added some planning comments to myboards.jsx

--- src/myboards/myboards.jsx
+++ src/myboards/myboards.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./myboards.css"
 
-export function Myboards() {
+export function Myboards() { //send in some list of all boards a person is a part of.
+    // create function to async create a board from an array.
+
+
   return (
     <main>
         <h2>My Boards</h2>
-
+        {/* have it create boards for every item on the list. */}
         
     </main>
   );
