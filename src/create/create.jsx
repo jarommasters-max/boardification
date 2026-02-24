f70b70d9871709d42af3d8bb0c3753b72b9ededd
Jarom Masters <jarommasters@gmail.com>
2026-02-23 17:59:14 -0700

    Worked on adding functionality to create board

--- src/create/create.jsx
+++ src/create/create.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import "./create.css";
 
 export function Create({user}) {
+    const [catrgory, setCategory] = React.useState('');
+    const [scoreType, setScoreType] = React.useState('');
+
+
+    function createBoard(){
+        
+    }
+
+    function getCategory(){
+        setCategory(txt.target.value);
+        
+    }
+
+    function getScoreType(){
+        setScoreType(txt.target.value);
+    }
+
   return (
     <main>
         <h2>Create New Leaderboard:</h2>
@@ -9,14 +26,14 @@ export function Create({user}) {
         <form method="get" action="myboards.html">
             <div>
                 <span>Board Title:</span>
-                <input type="text" className="form-control" placeholder="ex: Milk Drinking"/>
+                <input type="text" className="form-control" placeholder="ex: Milk Drinking" onChange={getCategory}/>
             </div>
             <div>
                 <span>Score Type:</span>
-                <input type="text" className="form-control" placeholder="ex: Litres of Milk"/>
+                <input type="text" className="form-control" placeholder="ex: Litres of Milk" onChange={getScoreType}/>
             </div>
             <div className="buttons">
-            <button type="submit" className="btn btn-outline-primary">Create Board</button>
+            <button type="submit" className="btn btn-outline-primary" onClick={createBoard}>Create Board</button>
             </div>
         </form>
         </main>
