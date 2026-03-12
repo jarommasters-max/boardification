b447f8af4dcd410873fc024821e6d66fc556d94d
Jarom Masters <jarommasters@gmail.com>
2026-03-11 22:10:37 -0600

    Added service dir, index.js, and vite.config.js

--- src/create/create.jsx
+++ src/create/create.jsx
@@ -8,57 +8,59 @@ export function Create({user,setTempBoard,setAllBoards,allBoards, setAllUsers, a
     const [scoreType, setScoreType] = React.useState('');
 
     const navigate = useNavigate();
-    
+
     if (loggedInUser){
-    function makeNewBoard(cat, scr, user){
-        const newBoard = {
-            id: crypto.randomUUID(),
-            title: cat,
-            typeScore: scr,
-            owner: user,
-            users: [user],
-            scores: {}
+
+    
+        function makeNewBoard(cat, scr, user){
+            const newBoard = {
+                id: crypto.randomUUID(),
+                title: cat,
+                typeScore: scr,
+                owner: user,
+                users: [user],
+                scores: {}
+            }
+            return (newBoard);
         }
-        return (newBoard);
-    }
 
 
-    function createBoard(){
-        const newBoard = makeNewBoard(category, scoreType, user);
-        setTempBoard(newBoard);
-        const updatedBoardSet = {...allBoards, [newBoard.id]: newBoard}
-        setAllBoards(updatedBoardSet);
-        localStorage.setItem('allBoards', JSON.stringify(updatedBoardSet));
-        navigate('/myboards');
-    }
+        function createBoard(){
+            const newBoard = makeNewBoard(category, scoreType, user);
+            setTempBoard(newBoard);
+            const updatedBoardSet = {...allBoards, [newBoard.id]: newBoard}
+            setAllBoards(updatedBoardSet);
+            localStorage.setItem('allBoards', JSON.stringify(updatedBoardSet));
+            navigate('/myboards');
+        }
 
-    function getCategory(txt){
-        setCategory(txt.target.value);
-        
-    }
+        function getCategory(txt){
+            setCategory(txt.target.value);
+            
+        }
 
-    function getScoreType(txt){
-        setScoreType(txt.target.value);
-    }
+        function getScoreType(txt){
+            setScoreType(txt.target.value);
+        }
 
-  return (
-    <main>
-    {user && <p>Logged in as: {user}</p>}
-        <h2>Create New Leaderboard:</h2>
-        
-            <div>
-                <span>Board Title:</span>
-                <input type="text" className="form-control" placeholder="ex: Milk Drinking" onChange={getCategory}/>
-            </div>
-            <div>
-                <span>Score Type:</span>
-                <input type="text" className="form-control" placeholder="ex: Litres of Milk" onChange={getScoreType}/>
-            </div>
-            <div className="buttons">
-            <button type="submit" className="btn btn-outline-primary" onClick={createBoard}>Create Board</button>
-            </div>
-        </main>
-  );
+    return (
+        <main>
+        {user && <p>Logged in as: {user}</p>}
+            <h2>Create New Leaderboard:</h2>
+            
+                <div>
+                    <span>Board Title:</span>
+                    <input type="text" className="form-control" placeholder="ex: Milk Drinking" onChange={getCategory}/>
+                </div>
+                <div>
+                    <span>Score Type:</span>
+                    <input type="text" className="form-control" placeholder="ex: Litres of Milk" onChange={getScoreType}/>
+                </div>
+                <div className="buttons">
+                <button type="submit" className="btn btn-outline-primary" onClick={createBoard}>Create Board</button>
+                </div>
+            </main>
+    );
 }
 else {
     function returnToLogin(){
