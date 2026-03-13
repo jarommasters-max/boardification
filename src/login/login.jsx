19f6c3e1648faf30bfefcef017d95c9279994bc5
Jarom Masters <jarommasters@gmail.com>
2026-03-12 19:34:06 -0600

    moved login function back, commented out old stuff

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -7,71 +7,101 @@ export function Login({getInfo, setAllUsers, allUsers, loggedInUser, setLoggedIn
     const [user, setUser] = React.useState('');
     const [password, setPassword] = React.useState('');
 
-const navigate = useNavigate();
+// const navigate = useNavigate();
 
-    function loginUser(usersToSearch = null) {
-        const list = usersToSearch || allUsers;
-        if (passwordChecks(list)){
-        localStorage.setItem('username', user);
-        localStorage.setItem('password', password); //remove this soon
-        getInfo(user);
-        setLoggedInUser(true)
-        navigate('/myboards');
-        return;
-        };
-        alert("Username or password are incorrect");
-        return;
+//     function loginUser(usersToSearch = null) {
+//         const list = usersToSearch || allUsers;
+//         if (passwordChecks(list)){
+//         localStorage.setItem('username', user);
+//         localStorage.setItem('password', password); //remove this soon
+//         getInfo(user);
+//         setLoggedInUser(true)
+//         navigate('/myboards');
+//         return;
+//         };
+//         alert("Username or password are incorrect");
+//         return;
+//     }
+
+//     function getUser(txt) {
+//         setUser(txt.target.value);
+//     }
+
+//     function getPassword(txt){
+//         setPassword(txt.target.value);
+//     }
+
+//     function passwordChecks(usersToSearch){
+//         if (Object.keys(usersToSearch).includes(user)){
+//             if(usersToSearch[user].pswrd === password){
+//             return true;
+//             };
+//         };
+//         return false;
+//     }
+
+//     function buildUser(user, password){
+//         const newUser = {
+//             username: user,
+//             pswrd: password,
+//             ownedBoards: [],
+//             participatingBoards: []
+//         };
+//         return newUser;
+//     }
+
+//     function addNewUser(){
+//         const newUser = buildUser(user, password);
+//         const updatedUserSet = {...allUsers, [user]: newUser};
+//         setAllUsers(updatedUserSet);
+//         return updatedUserSet;
+//     }
+
+//     function register(){
+//         const newUserList = addNewUser();
+//         loginUser(newUserList);
+//     }
+
+//     function signOut(){
+//         localStorage.removeItem('username');
+//         localStorage.removeItem('password')
+//         setLoggedInUser(false)
+//     }
+
+
+    async function loginUser() {
+    loginOrMakeUser(`/api/auth/login`);
+  }
+
+  async function makeUser() {
+    loginOrMakeUser(`/api/auth/create`);
+  }
+    
+    async function loginOrMakeUser(endpoint) {
+        const response = await fetch(endpoint, {
+            method: 'post',
+            body: JSON.stringify({
+                email: userName,
+                password: password
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        if (response?.status === 200) {
+            localStorage.setItem('userName', userName);
+            props.onLogin(userName);
+        }
+        else {
+      const body = await response.json();
+      setDisplayError(`Error: ${body.msg}`);
     }
-
-    function getUser(txt) {
-        setUser(txt.target.value);
-    }
-
-    function getPassword(txt){
-        setPassword(txt.target.value);
-    }
-
-    function passwordChecks(usersToSearch){
-        if (Object.keys(usersToSearch).includes(user)){
-            if(usersToSearch[user].pswrd === password){
-            return true;
-            };
-        };
-        return false;
-    }
-
-    function buildUser(user, password){
-        const newUser = {
-            username: user,
-            pswrd: password,
-            ownedBoards: [],
-            participatingBoards: []
-        };
-        return newUser;
-    }
-
-    function addNewUser(){
-        const newUser = buildUser(user, password);
-        const updatedUserSet = {...allUsers, [user]: newUser};
-        setAllUsers(updatedUserSet);
-        return updatedUserSet;
-    }
-
-    function register(){
-        const newUserList = addNewUser();
-        loginUser(newUserList);
-    }
-
-    function signOut(){
-        localStorage.removeItem('username');
-        localStorage.removeItem('password')
-        setLoggedInUser(false)
     }
 
     if (loggedInUser === false){
     return (
     <main>
-        <NotLoggedIn getUser={getUser} getPassword={getPassword} loginUser={loginUser} register={register}/>
+        <NotLoggedIn getUser={getUser} getPassword={getPassword} loginUser={loginUser} makeUser={makeUser}/>
     </main>
 
     
